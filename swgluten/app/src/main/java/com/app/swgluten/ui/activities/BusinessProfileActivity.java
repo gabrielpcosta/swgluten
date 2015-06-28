9c24060ff58d23b36681be3bf22243c3d32f94e4
Gabriel Pereira da Costa <gabrielp.costa@outlook.com>
2015-06-28 10:26:13 -0400

    Adicionado página de Perfil da Empresa.

--- swgluten/app/src/main/java/com/app/swgluten/ui/activities/BusinessProfileActivity.java
+++ swgluten/app/src/main/java/com/app/swgluten/ui/activities/BusinessProfileActivity.java
@@ -1,9 +1,11 @@
-package com.app.swgluten;
+package com.app.swgluten.ui.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.app.swgluten.R;
 
 
 public class BusinessProfileActivity extends ActionBarActivity {
