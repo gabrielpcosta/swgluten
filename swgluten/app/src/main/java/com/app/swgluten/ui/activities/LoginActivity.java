93643f2fbbc86cc8516c09cd2c95652b7505eea3
Gabriel Pereira da Costa <gabrielp.costa@outlook.com>
2015-06-28 00:02:29 -0400

    Adicionado activity de mapas.

--- swgluten/app/src/main/java/com/app/swgluten/ui/activities/LoginActivity.java
+++ swgluten/app/src/main/java/com/app/swgluten/ui/activities/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.app.swgluten.ui.activities;
 
+import android.app.Activity;
+
 /**
  * Created by GabrielPereira on 27/06/2015.
  */
-public class LoginActivity {
+public class LoginActivity extends Activity{
 }
